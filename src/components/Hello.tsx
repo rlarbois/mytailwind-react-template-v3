8a4b56266d9910e397beac8bf3b13e20d0565aef
rlarbois <76477058+rlarbois@users.noreply.github.com>
2022-03-04 12:05:59 +0800

    React Tailwin V3 Template

---
+++ src/components/Hello.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <div className="p-10 min-h-screen flex items-center justify-center bg-cool-gray-700">
+    <h1 className="text-9xl font-black text-white text-center">
+      <span className="bg-gradient-to-r text-transparent bg-clip-text from-green-400 to-purple-500">
+        Hello World
+      </span>
+    </h1>
+    
+  </div>
+  )
+}
+
+export default Hello
